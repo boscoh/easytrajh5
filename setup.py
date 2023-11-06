295bb46691368f2ce56cab25f11f8d5f1d3fcb53
Bosco Ho <apposite@gmail.com>
2023-11-06 18:30:39 +1100

    fix: redo setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
-#!/usr/bin/env python
-
 import setuptools
 
-# to allow pip to install in editable mode
-if __name__ == "__main__":
-    setuptools.setup()
+setuptools.setup(
+    name="easytrajh5",
+    version="0.1",
+    author="Bosco Ho",
+    author_email="bosco@redesignscience.com",
+    description="Redesign Science Cloud Job Client in Python",
+    install_requires=[
+        "addict",
+        "deepdiff",
+        "h5py",
+        "mdtraj",
+        "numpy",
+        "orjson",
+        "ParmEd",
+        "pydash",
+        "rich",
+        "ruyaml"
+
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+    include_package_data=True,
+    package_data={
+        "easytrajh5": [
+            "easytrajh5/data/*",
+        ]
+    },
+)
