d0b48adbec481446921299ef5bce745be323017e
Bosco Ho <apposite@gmail.com>
2023-12-05 11:19:50 +1100

    feat: get_attr_dict

--- easytrajh5/h5.py
+++ easytrajh5/h5.py
@@ -196,6 +196,13 @@ class EasyH5File:
             v = float(v)
         return v
 
+    def get_attr_dict(self, dataset_key=None) -> dict:
+        """Returns h5py attrs as a dict"""
+        return {
+            k: self.get_attr(key, dataset_key)
+            for key in self.get_attr_keys(dataset_key)
+        }
+
     def set_attr(self, key, value, dataset_key=None):
         attrs = self.get_attrs(dataset_key)
         if key in attrs:
